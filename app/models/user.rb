db07350d85a2f44ffe5aca05bc37b6bcb583f03f
EC2 Default User <ec2-user@ip-172-31-60-6.ec2.internal>
2021-06-03 12:16:37 +0000

    update6-3

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   attachment :profile_image
   has_many :books, dependent: :destroy
+  has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   validates :name, uniqueness: true, length: {minimum: 2,maximum: 20}
   validates :introduction, length: {maximum: 50}
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,11 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
   devise_for :users
-  resources :books
+  resources :books do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  
+   resource :favorite, only: [:create, :destroy]
+   resource :book_comment, only: [:create]
+  end
+  resources :book_comments, only: [:destroy]
   resources :users, only: [:show, :edit, :update, :index]
 end
